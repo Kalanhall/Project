f9df854b825ca204dd2c133b2b3af6726f86ea6f
692015 <wujm002@galanz.com>
2020-08-31 19:15:08 +0800

    no message

--- Project/AppDelegate.swift
+++ Project/AppDelegate.swift
@@ -7,7 +7,7 @@
 //
 
 import UIKit
-import YKWoodpecker
+//import YKWoodpecker
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -20,7 +20,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         window?.rootViewController = UITabBarController().setupItems()
         window?.makeKeyAndVisible()
         
-        YKWoodpeckerManager.sharedInstance()?.show()
+//        YKWoodpeckerManager.sharedInstance()?.show()
 
         return true
     }
